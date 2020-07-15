b8daea4f4360106ea3a3182f5920850786a76777
mrChernicharo <felipe.chernicharo@gmail.com>
2020-07-15 17:11:14 -0300

    property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core'
 
 @Component({
-  // selector: '.app-server',
-  // selector: '[app-server]',
   selector: 'app-server',
   templateUrl: './server.component.html',
   styles: [`
